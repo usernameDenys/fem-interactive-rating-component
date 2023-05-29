458a93dd91fb242c6d9edc518c73609ff1e71c47
Denys <133951627+usernameDenys@users.noreply.github.com>
2023-05-29 12:07:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const submitButton = document.querySelector('.button');
 const gratitudeState = document.querySelector('.container-gratitude-state');
 const ratingState = document.querySelector('.container-rating-state');
 
+function reply_click(clicked_id)
+{
+    let ratingResult = clicked_id;
+    const ratingSelected = document.querySelector('.rating-selected');
+    ratingSelected.innerHTML += 'You selected '+ ratingResult + ' out of 5';
+}
 
 function submitingResult () {
     ratingState.style.display = 'none';
     gratitudeState.style.display = 'flex';
-
-};
+}
 
 submitButton.addEventListener('click', submitingResult);
